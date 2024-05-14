f91da9086cb848c4d6dc59d24c23f5d4273f8be4
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-05-14 15:03:55 +0200

    Update api.ts

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,13 +1,14 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-const fetchImages = async (searchInput: string) => {
+const fetchImages = async (searchInput: string): Promise<ImageData[]> => {
   const apiKey = "soIIhOVxblDhlozBu-t3ph783hav1g27jQlrq9LvcmI";
   const perPage = 10;
   const url = `https://api.unsplash.com/search/photos?query=${searchInput}&client_id=${apiKey}&per_page=${perPage}`;
 
-  const response = await axios.get(url);
+  const response: AxiosResponse<{ results: ImageData[] }> = await axios.get<{ results: ImageData[] }>(url);
 
   return response.data.results;
 };
 
 export { fetchImages };
+
